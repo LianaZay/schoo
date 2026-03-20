0930dce50102ee288542d6c2e3223843ef302961
LianaZay <zaytseva.l@ssmg.cz>
2026-03-20 08:38:09 +0100

    Add files via upload

--- 18.03.py
+++ 18.03.py
@@ -1,47 +1,34 @@
-
+import random
 def otz(o):
-    with open("khlgjkh") as f:
-        o = otz.split("|")
-        yp = o[0].split(";")
-        moznst = o[1].ssplit("|")
-        otazk = yp[0]
-        odp = yp[1]
-        otz(o)
-        print(o)
-        return o
+
+    x = o.split(";")
+    yp = o[0].split("|")
+
+    global moznst, otazk, odp
+    moznst = x[1].ssplit("|")
+    otazk = yp[0]
+    odp = int(yp[1])
+
+def hghfg():
+    global polrad
+    with open("khlgjkh", "r") as l:
+        polrad = l.readlines()
+
+def nahrad():
+        k = random.randint(0, len(polrad))
+        return polrad[k]
 
 
 
+otazk = ""
+moznst = []
+odp = 0
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+otz(otazk)
+print(otazk)
+print(moznst)
+print(odp)
 
 
 
